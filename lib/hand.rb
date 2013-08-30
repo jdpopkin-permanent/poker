ec98d598e625c8d9139badbf59e3f64f94b0ccd5
App Academy Student <student@appacademy.io>
2013-08-30 17:12:24 -0400

    Now evaluates hands

--- lib/hand.rb
+++ lib/hand.rb
@@ -3,6 +3,46 @@ require "cards.rb"
 module Poker
   def evaluate
     self.cards.sort_by! { |card| card.value }
+    # straight flush
+    if flush? && straight?
+      return [8] + priority_ordered
+    end
+
+    if n_of_a_kind?(4)
+      return [7] + priority_ordered
+    end
+
+    if n_of_a_kind?(3) && n_of_a_kind?(2)
+      return [6] + priority_ordered
+    end
+
+    if flush?
+      return [5] + priority_ordered
+    end
+
+    if straight?
+      return [4] + priority_ordered
+    end
+
+    if n_of_a_kind?(3)
+      return [3] + priority_ordered
+    end
+
+    if two_pair?
+      return [2] + priority_ordered
+    end
+
+    if n_of_a_kind?(2)
+      return [1] + priority_ordered
+    end
+
+    return [0] + priority_ordered
+  end
+
+  def priority_ordered
+    grouped_by_value.reverse.sort_by do |group|
+      -1 * group[0].value * 20 ** group.count
+    end.flatten.map(&:value)
   end
 
   def flush?
@@ -16,6 +56,43 @@ module Poker
       return false unless offset + i == card.value
     end
     true
+  end
+
+  def n_of_a_kind?(n)
+    grouped_by_value.any? do |group|
+      group.count == n
+    end
+  end
+
+  def two_pair?
+    value_groups = grouped_by_value
+    num_pairs = 0
+
+    value_groups.each do |group|
+      num_pairs += 1 if group.count == 2
+    end
+
+    num_pairs == 2
+  end
+
+  def highest_val(card_array)
+    card_array.last.value # note: assumes sorted. probably safe.
+  end
+
+
+
+  private
+
+  def grouped_by_value
+    grouped = [[@cards[0]]]
+    @cards[1..-1].each_with_index do |card, i|
+      if grouped[-1][0].value == card.value
+        grouped[-1] << card
+      else
+        grouped << [card]
+      end
+    end
+    grouped
   end
 end
 
--- spec/hand_spec.rb
+++ spec/hand_spec.rb
@@ -105,4 +105,154 @@ describe Hand do
       expect(hand.straight?).to be_false
     end
   end
+
+  describe "#n_of_a_kind" do
+    let(:deck) { double("deck") }
+    subject(:hand) { Hand.new(deck) }
+
+    it "returns true if 4 of a kind with arg 4" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(2, :heart), Card.new(2, :clud), Card.new(6, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.n_of_a_kind?(4)).to be_true
+    end
+
+    it "returns false if 4 of a kind with arg 3" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(2, :heart), Card.new(2, :clud), Card.new(6, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.n_of_a_kind?(3)).to be_false
+    end
+
+
+    it "returns false if all cards are unique" do
+      five_cards = [Card.new(2, :diamond), Card.new(3, :spade),
+        Card.new(4, :spade), Card.new(5, :spade), Card.new(6, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.n_of_a_kind?(2)).to be_false
+    end
+  end
+
+  describe "#two_pair?" do
+    let(:deck) { double("deck") }
+    subject(:hand) { Hand.new(deck) }
+
+    it "returns true if two distinct pairs exist" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(3, :heart), Card.new(3, :club), Card.new(6, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.two_pair?).to be_true
+    end
+
+    it "returns false for full house" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(3, :heart), Card.new(3, :club), Card.new(3, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.two_pair?).to be_false
+    end
+
+    it "returns false when no pairs exist" do
+      five_cards = [Card.new(4, :diamond), Card.new(2, :spade),
+        Card.new(3, :heart), Card.new(5, :club), Card.new(6, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.two_pair?).to be_false
+    end
+  end
+
+  describe "#highest_val" do
+    let(:deck) { double("deck") }
+    subject(:hand) { Hand.new(deck) }
+
+    it "returns value of highest card" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(3, :heart), Card.new(3, :club), Card.new(6, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.highest_val(hand.cards)).to eq(6)
+    end
+  end
+
+  describe "#evaluate" do
+    let(:deck) { double("deck") }
+    subject(:hand) { Hand.new(deck) }
+
+    it "returns 0 for High Card" do
+      five_cards = [Card.new(2, :diamond), Card.new(4, :spade),
+        Card.new(6, :heart), Card.new(8, :club), Card.new(10, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([0, 10, 8, 6, 4, 2])
+    end
+
+    it "returns 1 for One Pair" do
+      five_cards = [Card.new(2, :diamond), Card.new(4, :spade),
+        Card.new(6, :heart), Card.new(10, :club), Card.new(10, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([1, 10, 10, 6, 4, 2])
+    end
+
+    it "returns 2 for Two Pair" do
+      five_cards = [Card.new(4, :diamond), Card.new(4, :spade),
+        Card.new(6, :heart), Card.new(10, :club), Card.new(10, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([2, 10, 10, 4, 4, 6])
+    end
+
+    it "returns 3 for Three of a Kind" do
+      five_cards = [Card.new(4, :diamond), Card.new(4, :spade),
+        Card.new(6, :heart), Card.new(4, :club), Card.new(10, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([3, 4, 4, 4, 10, 6])
+    end
+
+    it "returns 4 for a Straight" do
+      five_cards = [Card.new(4, :diamond), Card.new(5, :spade),
+        Card.new(6, :heart), Card.new(7, :club), Card.new(8, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([4, 8, 7, 6, 5, 4])
+    end
+
+    it "returns 5 for a Flush" do
+      five_cards = [Card.new(2, :spade), Card.new(5, :spade),
+        Card.new(6, :spade), Card.new(7, :spade), Card.new(8, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([5, 8, 7, 6, 5, 2])
+    end
+
+    it "returns 6 for a Full House" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(2, :club), Card.new(8, :diamond), Card.new(8, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([6, 2, 2, 2, 8, 8])
+    end
+
+    it "returns 7 for Four of a Kind" do
+      five_cards = [Card.new(2, :diamond), Card.new(2, :spade),
+        Card.new(2, :club), Card.new(2, :heart), Card.new(8, :spade)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([7, 2, 2, 2, 2, 8])
+    end
+
+    it "returns 8 for Straight Flush" do
+      five_cards = [Card.new(13, :diamond), Card.new(12, :diamond),
+        Card.new(11, :diamond), Card.new(10, :diamond), Card.new(9, :diamond)]
+      deck.stub(:deal).and_return(five_cards.dup)
+
+      expect(hand.evaluate).to eq([8, 13, 12, 11, 10, 9])
+    end
+  end
+
 end
